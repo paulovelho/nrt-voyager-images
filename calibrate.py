7f312dcac17b0965a8f7dabe888f5640d9ee08ec
Paulo Martins <paulo.martins@vokal.io>
2021-06-06 17:19:38 -0300

    python calibrations

---
+++ calibrate.py
@@ -0,0 +1,102 @@
+import sys
+import json
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+from pprint import pprint
+
+
+def plot_wave(wave):
+	plt.plot(wave);
+	plt.title('analyzing file {}.wav'.format(filename));
+	plt.show();
+
+
+def parse_data(filename, peak, valley):
+	data = [];
+	times = [];
+	with open('data/' + filename + '.txt', "r") as file:
+		for line in file:
+			c = line.split('\t')
+			times.append( int(c[0]) )
+			data.append( int(c[1].rstrip("\n")) )
+
+	wave_start = 0;
+	wave_arr = [];
+	timestamp = '';
+
+	wave_sizes = [];
+	wave_times = [];
+	arr = [];
+
+	for i in range(len(data)):
+		d = data[i];
+		t = times[i];
+		if(wave_start == 0):
+			if(d < valley):
+				wave_start = i;
+				wave_arr = [];
+				timestamp = str(t);
+		else:
+			if(d > peak):
+				wave_sizes.append(len(wave_arr))
+				timestamp = timestamp + '-' + str(t);
+				print('wave {} goes from {} with {} points'.format(len(arr), timestamp, len(wave_arr)));
+				wave_times.append(timestamp);
+				arr.append(wave_arr);
+				wave_start = 0;
+				wave_arr = [];
+			else:
+				wave_arr.append(d);
+
+
+	# delete problematic nodes
+#	del(arr[len(arr)-1]);
+#	del(arr[0]);
+
+	wave_avg = math.floor( sum(wave_sizes) / len(wave_sizes) )
+	max_len = np.max([len(a) for a in arr])
+	print('had {} waves with an avg of {} points and a max of {}'.format(len(arr), wave_avg, max_len));
+
+	arr = np.asarray([np.pad(a, (0, max_len - len(a)), 'constant', constant_values=0) for a in arr])
+	print('after conversion, there are {} waves'.format(len(arr)));
+
+	analyze(arr, wave_times);
+#	plot_image(arr);
+
+
+def analyze(data, times):
+
+	print('wave 0 has {} points'.format(len(data[0])));
+
+	max_wave = 0;
+	max_wave_index = -1;
+	for i in range(len(data)):
+		wave = data[i];
+		if(len(wave) > max_wave):
+			max_wave = len(wave);
+			max_wave_index = i;
+
+	print('biggest wave is {}, at {}, with {}'.format(max_wave_index, times[max_wave_index], len(data[max_wave_index])))
+	max_len = np.max([a for a in data[max_wave_index]])
+	print('max = {} (peak: {})'.format(max_len, peaks));
+
+	plot_wave(data[max_wave_index]);
+
+
+if __name__ == '__main__':
+	if len(sys.argv) < 3:
+		print('Missing information to work with')
+		print('Usage: python voyager.py [file name (inside ./data)] [peaks] [valleys]')
+		exit()
+	filename = sys.argv[1]
+	try:
+		peaks = sys.argv[2]
+		valleys = sys.argv[3]
+	except IndexError:
+		print('Index Error!');
+
+	parse_data(filename, int(peaks), int(valleys))
+
--- voyager.py
+++ voyager.py
@@ -8,9 +8,52 @@ import matplotlib.cm as cm
 from pprint import pprint
 
 
-def plot(data):
-#	nrows, ncols = 768, 1024
-#	data = data.reshape((nrows, ncols))
+def parse_data(filename, peak, valley, fix):
+	data = [];
+	times = [];
+	with open('data/' + filename + '.txt', "r") as file:
+		for line in file:
+			c = line.split('\t')
+			times.append( int(c[0]) )
+			data.append( int(c[1].rstrip("\n")) )
+
+	wave_start = 0;
+	wave_sizes = [];
+	wave_arr = [];
+	arr = [];
+
+	for i in range(len(data)):
+		d = data[i];
+		if(wave_start == 0):
+			if(d < valley):
+				wave_start = i;
+				wave_arr = [];
+			else:
+				continue;
+		else:
+			wave_arr.append(float(d));
+			if(d > peak):
+				print('wave ending at {} with {} points'.format(i, len(wave_arr)));
+				wave_start = 0;
+				if(fix > 0):
+					if(len(wave_arr) > fix): continue;
+				wave_sizes.append(len(wave_arr))
+				arr.append(wave_arr);
+
+	# delete problematic nodes
+	del(arr[0]);
+	del(arr[len(arr)]);
+
+	arr = np.array(arr)
+
+	plot_image(arr);
+
+def plot_image(data):
+	data = data.transpose();
+
+	print('mid point row: ', len(data)/2);
+	pprint(data[int(len(data)/2)]);
+
 	print('plotting {} points'.format(len(data)))
 
 #	data = np.split(np.array(data), 512)
@@ -19,145 +62,22 @@ def plot(data):
 	plt.show()
 
 
-
-def fix_json(filename, time, start, end):
-	times = []
-	data = []
-	with open('data/' + filename, "r") as file:
-		for line in file:
-			c = line.split('\t')
-			times.append( float(c[0]) )
-			data.append( float(c[1].rstrip("\n")) )
-
-	data = scale_data(data)
-#	soundFile([i for i in data], time, start, end)
-#	checkValidStream(data, times, time, start, end)
-	imageFile(data, times, time, start, end)
-
-def scale_data(arr):
-	print('scaling wave')
-	digits = 4
-	max_val = float(max(arr))
-	new_data = []
-	for x in arr:
-		new_data.append(round(x / max_val, digits))
-
-	return new_data
-
-def soundFile(arr, time, start, end):
-	json_content = {}
-	json_content["time"] = time
-	json_content["scale"] = len(arr) / time
-	json_content["img_start"] = start
-	json_content["img_end"] = end
-	# we don't need a too much detailed array to show
-	del arr[::2]
-	del arr[::2]
-	json_content["data"] = arr
-	json_content["points"] = len(arr)
-
-	file_content = json.dumps(json_content, separators=(',', ':'))
-
-	name = 'peaks/' + filename.split('.')[0] + '.json'
-	print('\n------------------------------------') 
-	print('-- generating wavesurfer file: ' + name + ' --')
-	print('------------------------------------\n') 
-
-	with open(name, "w") as f:
-		f.write(file_content)
-
-def imageFile(data, times, time, start, end):
-	arr = [];
-	valid_time = end - start;
-	wave_length = 10
-
-	print('analyzing data from wave of {}, starting at {} and ending at {}'.format(time, start, end))
-
-	wave_start = 0;
-	wave_end = end;
-	wave_arr = [];
-	waves = 0;
-
-	peak = 200;
-	valley = 60;
-
-	wave_size = [];
-
-	print('times: {} '.format(len(times)))
-
-	for i in range(len(times)):
-		t = int(times[i]*1000)
-		if(t <= start): continue
-		if(t > end): continue
-
-		d = normalizeColor(data[i])
-		if(wave_start == 0):
-			if(d < valley):
-				wave_start = int(t);
-				wave_arr = [];
-				wave_end = t + wave_length;
-				if(wave_end > end): 
-					end = t
-					continue
-				print('wave starting at {}; will end at {}'.format(t, wave_end));
-			else:
-				continue
-
-		wave_arr.append(d);
-		if(wave_start > 0 and d > peak):
-			print('wave ending at {} with {} points'.format(t, len(wave_arr)));
-			wave_size.append(len(wave_arr))
-			wave_start = 0;
-			arr.append(wave_arr);
-			waves = waves + 1;
-
-	del arr[0]
-	wave_avg = math.floor( sum(wave_size) / len(wave_size) )
-
-	max_len = np.max([len(a) for a in arr])
-	arr = np.asarray([np.pad(a, (0, max_len - len(a)), 'constant', constant_values=0) for a in arr])
-
-	print('had {} waves with a max of {}'.format(waves, max_len))
-	arr = np.array(arr)
-	plot(arr.transpose())
-
-
-def checkValidStream(data, times, time, start, end):
-	arr = [];
-	wave_start = 0;
-	for i in range(len(times)):
-		t = int(times[i]*1000)
-		if(t <= start): continue
-		if(t > end): continue
-		d = normalizeColor(data[i])
-		arr.append(d);
-
-	max_value = np.max(arr)
-	min_value = np.min(arr)
-
-	print('got {} points, max: {}, min: {}'.format(len(arr), max_value, min_value))
-
-
-def normalizeColor(c):
-	# color = between(-1 and 1)
-	c = c + 1; # color = between(0 and 2)
-	return int(round((256 * c) / 2))
-
 if __name__ == '__main__':
 	if len(sys.argv) < 3:
 		print('Missing information to work with')
-		print('Usage: python fix-json.py [file name (inside ./data)] [time] [start] [end]')
+		print('Usage: python voyager.py [file name (inside ./data)] [peaks] [valleys]')
 		exit()
-	filename = sys.argv[1]
-	time = sys.argv[2]
+	filename = sys.argv[1];
 	try:
-		start = sys.argv[3]
+		peaks = sys.argv[2];
+		valleys = sys.argv[3];
 	except IndexError:
-		start = 0
-	try:
-		end = sys.argv[4]
-	except IndexError:
-		end = time
+		print('Index Error!');
 
-	fix_json(filename, int(time), int(start), int(end))
+	try:
+		calibration_fix = sys.argv[4];
+	except IndexError:
+		calibration_fix = 0;
+
+	parse_data(filename, int(peaks), int(valleys), int(calibration_fix));
 
